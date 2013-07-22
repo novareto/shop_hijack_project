3a2f09d5893ae1d14b417a60acd53f9da339b6c8
Souheil CHELFOUH <trollfot@gmail.com>
2013-07-22 15:23:38 +0200

    changing imports to use uvclight

--- src/shop_hijack/src/shop_hijack/browser/viewlets.py
+++ src/shop_hijack/src/shop_hijack/browser/viewlets.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from dolmen.viewlet import Viewlet
 from dolmen.breadcrumbs import BreadcrumbsRenderer
-from cromlech.browser import slot
-from grokcore.security import require
 from uvc.tb_layout.managers import AboveContent
+from uvclight import Viewlet, viewletmanager, require
 
 
 class Breadcrumbs(BreadcrumbsRenderer, Viewlet):
     require('zope.Public')
-    slot(AboveContent)
+    viewletmanager(AboveContent)
     
     def __init__(self, *args, **kws):
         Viewlet.__init__(self, *args, **kws)
